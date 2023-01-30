69754c908079ae9193987a8fff6144d94e9b956e
MirrorOfLourence <90408662+MirrorOfLourence@users.noreply.github.com>
2023-01-30 21:00:20 +0200

    Add files via upload

---
+++ semester2/lab1/main.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <vector>
+
+class Point	//класс точки
+{
+public:
+	Point();
+	Point(int x, int y)
+		:x(x), y(y)
+	{
+	}
+	int getX() {return x; }
+	int getY() {return y; }
+private:
+	int x;
+	int y;
+};
+
+float getAngleFirst(Point &A, Point &B)
+{
+	if((A.getX() == B.getX()))	//если точки A и B совпадают или составляют вертикальный вектор
+		return 0;
+	if((A.getY() == B.getY()))	//если точки A и B составляют горизонтальный вектор
+		return 1;
+	Point v1(1,0);		//точками задаём векторы: горизонтальный
+	Point v2(A.getX() - B.getX(), A.getY() - B.getY());	//и из данных точек
+	return ( (v1.getX()*v2.getX() + v1.getY()*v2.getY())	//вычисляем cos угла между v1 и v2
+		/ (sqrt(v1.getX()*v1.getX() + v1.getY()*v1.getY()) * sqrt(v2.getX()*v2.getX() + v2.getY()*v2.getY())));
+}
+
+float getAngle(Point &A, Point &B, Point &C)
+{
+	Point v1(C.getX() - B.getX(), C.getY() - B.getY());		//точками задаём векторы: 1 из данных точек (BC)
+	Point v2(A.getX() - B.getX(), A.getY() - B.getY());	//и из данных точек (BA)
+	return ( (v1.getX()*v2.getX() + v1.getY()*v2.getY())	//вычисляем cos угла между v1 и v2
+		/ (sqrt(v1.getX()*v1.getX() + v1.getY()*v1.getY()) * sqrt(v2.getX()*v2.getX() + v2.getY()*v2.getY())));
+}
+
+void show( std::vector<Point> &v )
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		std::cout << "(" << v[i].getX() << ", " << v[i].getY() << ")" << std::endl;
+	}
+}
+
+void checkShell( std::vector<Point> &v )	//ф-ия проверки существования выпуклой оболочки
+{
+	int yMin = INT_MAX, current = 0, numOperated = 0, length = v.size();	
+	//numOperated - число включённых в оболочку точек; current - текущая точка(в 1 for - нижняя точка)
+	std::vector<Point> answ;
+	for (int i = 0; i < v.size(); i++)	//ищем нижнюю точку из заданных
+	{
+		if (v[i].getY() < yMin)
+		{
+			yMin = v[i].getY();
+			current = i;
+		}
+		else if ((v[i].getY() == yMin) && (v[i].getX() < v[current].getX()))	//если нашли нижнюю, но более левую точку
+			current = i;
+	}
+	if (yMin == INT_MAX)	//если нижней не нашлось, выходим из функции
+	{
+		std::cout << "Нельзя построить выпуклую оболочку!" << std::endl;
+		return;
+	}
+	else	//иначе продолжаем проверку
+	{
+		answ.push_back(v[current]);		//добавим в ответ найденную вершину
+		std::swap( v[current], v[length - 1] );	//переносим нижнюю точку в конец вектора
+		numOperated++;
+
+		float maxPr = 0;	//maxPr - максимальное значение при cos(a) 
+		bool firstAngle = true;
+		current = length-numOperated;
+
+		while (true)
+		{
+			if (firstAngle)	//если обрабатываем первый угол, сравниваем с горизонталью
+			{
+				for (int i = 0; i < length-numOperated; i++)
+				{
+					float value = getAngleFirst(v[i], v[length-numOperated]);
+					if (value > maxPr)
+					{
+						maxPr = value;
+						current = i;
+					}
+				}
+				answ.push_back(v[current]);		//добавим в ответ найденную вершину
+				numOperated++;	//инкремент количества обработанных точек
+				std::swap( v[current], v[length - numOperated] );	//переносим нижнюю точку в конец вектора
+
+				maxPr = 0;
+				firstAngle = false;	//снимает флаг первого угла
+			}
+			else	//иначе сравниваем с новой прямой, составленной из двух предыдущих добавленных точек
+			{
+				for (int i = 0; i < length-numOperated; i++)
+				{
+					float value = getAngle(v[length-numOperated], v[length-numOperated+1], v[i]);
+					if (value > maxPr)
+					{
+						maxPr = value;
+						current = i;
+					}
+					else if (value == maxPr)
+					{
+						if (v[i].getX() > v[current].getX())
+						{
+							maxPr = value;
+							current = i;
+						}
+					}
+					if (i == length - numOperated - 1)
+					{
+						value = getAngle(v[length-numOperated], v[length-numOperated+1], v[length-1]);
+						if (value > maxPr)
+						{
+							std::cout << "Выпуклая оболочка состоит из точек: " << std::endl;
+							show(answ);
+							return;
+						}
+					}
+				}
+				answ.push_back(v[current]);		//добавим в ответ найденную вершину
+				numOperated++;	//инкремент количества обработанных точек
+				std::swap( v[current], v[length - numOperated] );	//переносим нижнюю точку в конец вектора
+				maxPr = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	std::vector<Point> vertexes;
+
+	int counter;
+	std::cout << "Сколько точек хотите ввести?" << std::endl;
+	std::cin >> counter;
+
+	for (int i = 0; i < counter; i++)
+	{
+		int x=0,y=0;
+		std::cout << "Введите координату X: ";
+		std::cin >> x;
+		std::cout << "Введите координату Y: ";
+		std::cin >> y;
+		std::cout << std::endl;
+
+		vertexes.push_back(Point(x,y));
+	}
+	if (vertexes.size() > 2)
+	{
+		checkShell(vertexes);
+	}
+	else
+	{
+		std::cout << "Нельзя построить выпуклую оболочку!" << std::endl;
+	}
+	return 0;
+}
---
+++ semester2/lab2/main.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <fstream>
+
+//нумерация вершин начинается с 0
+void BFS(std::vector<std::vector<int>>& graph)  //ф-ия поиска 
+{
+    int length = graph[0].size();
+    std::queue<int> q;  //очередь для обработки вершин
+    std::vector<bool>  used(length);   //вектор использованных вершин
+    std::vector<int> dist(length);
+    
+    //обрабатываем начальную вершину
+    q.push(0);
+
+    while (!q.empty())   //пока очередь не пуста, выполняем обработку графа
+    {
+        int current = q.front();    //вытаскиваем текущую вершину из очереди
+        q.pop();
+        used[current] = true;
+
+        for (int i = 0; i < length; i++)    //смотрим соседние к текущей вершины
+        {
+            if ((graph[current][i] == 1) && (used[i] == false)) //если нашли непомеченную ещё соседнюю вершину, 
+            {
+                q.push(i);  //добавляем её в очередь
+                dist[i] = dist[current] + 1;    //расстояние до неё = расстоянию до предыдущей +1
+            }
+        }
+    }
+    for (int i = 1; i < length; i++)
+    {
+        std::cout << "Расстояние от вершины А до вершины " << (char)(65+i) << ":\t" << dist[i] << std::endl;
+    }
+
+}
+
+int main()
+{ 
+    setlocale(LC_ALL, "Rus");
+    std::vector<std::vector<int>> graph;    //Граф
+    
+    std::ifstream in;   //поток на вход
+    in.open("input.txt");   //загружаем в поток файл ввода
+    if (in.is_open())   //если файл открылся, то
+    {
+        char c; //сюда считываем очередной символ
+        std::vector<int> line;  //вектор, в который загружаем строку
+        for (c = in.get(); c!= EOF; c = in.get())   //проходим циклом до конца файла
+        {
+            if (c == '\n')  //если дошли до конца строки, пушим её в вектор
+            {
+                graph.push_back(line);
+                line.clear();
+            }
+            else    //иначе пушим в строку очередной символ
+            {
+                line.push_back(c - '0');
+            }
+            
+        }
+        graph.push_back(line);  //пушим в вектор последнюю строку, т.к. не обрабатывается в цикле из-за EOF в конце файла
+        line.clear();
+    }
+    else    //если файл не открылся, то
+    {
+        std::cout << "ОШИБКА: Не удалось открыть файл!" << std::endl;   //выдаём ошибку
+    }
+
+    BFS(graph);
+
+    return 0;
+}
