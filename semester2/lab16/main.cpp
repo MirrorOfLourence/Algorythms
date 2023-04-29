b07dd3c1d54b992b8c291ce20efde48a4e7b24a0
MirrorOfLourence <90408662+MirrorOfLourence@users.noreply.github.com>
2023-04-29 10:42:19 +0200

    Add files via upload

---
+++ semester2/lab16/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> A;
+std::vector<int> answer;
+std::vector<int> weights;
+std::vector<int> prices;
+
+void findComplect(int k, int s)
+{
+    if(A[k][s] == 0)
+        return;
+    if(A[k-1][s] == A[k][s])
+        findComplect(k-1, s);
+    else
+    {
+        findComplect(k-1, s - weights[k]);
+        answer.push_back(k);
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int weight, price, n = 0;
+    std::cout << "Введите вес и стоимость предмета(для прекращения введите отрицательные значения): " << std::endl;
+    std::cout << ++n << ": ";
+    std::cin >> weight >> price;
+    while (weight > 0 && price > 0)
+    {
+        weights.push_back(weight);
+        prices.push_back(price);
+        std::cout << ++n << ": ";
+        std::cin >> weight >> price;
+    }
+    std::cout << "Введите максимальный вес: ";
+    std::cin >> weight; 
+    std::cout << std::endl;
+
+    A.resize(weights.size(), std::vector<int>(weight, 0));
+    for (int i = 0; i < weights.size(); i++)
+    {
+        for (int j = 0; j < weight; j++)
+        {
+            if(weights[i] <= j+1) 
+            {
+                if(i == 0)
+                    A[i][j] = prices[i];
+                else
+                { 
+                    if(j == 0) 
+                        A[i][j] = prices[i];
+                    else
+                        A[i][j] = (A[i-1][j] > A[i-1][j-1]+prices[i]) ? A[i-1][j] : A[i-1][j-1]+prices[i];
+                }
+            }
+        }
+    }
+    std::cout << "Максимальная стоимость: " << A[weights.size()-1][weight-1] << std::endl;
+    /*for (int i = 0; i < answer.size(); i++)
+    {
+        std::cout << "Предмет: " << answer[i] << "\tСтоимость: " << prices[answer[i]] << std::endl;
+    }*/
+    return 0;
+}
---
+++ semester2/lab17/main.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<int> weights;
+std::vector<std::vector<int>> containers;
+std::vector<int> weightInContainer;
+std::vector<bool> packed;
+
+void packByBoxes(int weight)
+{
+    for (int i = 0; i < packed.size(); i++)
+    {
+        if (!packed[i])
+        {
+            if (containers.size() == 0) //Обработка первого контейнера
+            {
+                if(weights[i] <= weight)    //Если предмет вмещается в контейнер, то пакуем его 
+                {
+                    containers.push_back(std::vector<int> {i});
+                    weightInContainer.push_back(weights[i]);
+                    packed[i] = true;
+                }
+                else    //Иначе помечаем предмет как упакованный, но не пакуем
+                {
+                    packed[i] = true;
+                }
+            }
+            else    //Если контейнер не первый
+            {
+                for (int j = 0; j < containers.size(); j++)   //Перебираем контейнеры, ищем первый, в который поместится
+                {
+                    if (weights[i] <= weight - weightInContainer[j])    //если предмет помещается, кладём его и прерываем цикл
+                    {
+                        containers[j].push_back(i);
+                        weightInContainer[j] += weights[i];
+                        packed[i] = true;
+                        break;
+                    }
+                }
+                if (!packed[i])  //если не нашлось ящика, способного вместить очередной предмет
+                {
+                    containers.push_back(std::vector<int> {i});
+                    weightInContainer.push_back(weights[i]);
+                    packed[i] = true;
+                }
+
+            }
+        }
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int weight, n = 0;
+    std::cout << "Введите вес предмета(для прекращения введите отрицательное значение): " << std::endl;
+    std::cout << ++n << ": ";
+    std::cin >> weight;
+    while (weight > 0)
+    {
+        weights.push_back(weight);
+        std::cout << ++n << ": ";
+        std::cin >> weight;
+    }
+    std::cout << "Введите вместимость контейнеров: ";
+    std::cin >> weight; 
+    std::cout << std::endl;
+
+    n--;    //Заполняем векторы предварительными данными
+    packed.resize(n, false);
+
+
+    packByBoxes(weight);
+    for (int i = 0; i < containers.size(); i++)
+    {
+        std::cout << "Контейнер №" << i << ": ";
+        for (int item : containers[i])
+        {
+            std::cout << weights[item] << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
